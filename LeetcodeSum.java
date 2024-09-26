98c81848935240ff8741085e4e24310f81d838c5
Abhinav kumar <110728413+ABHINAV12KUMAR@users.noreply.github.com>
2024-09-27 00:16:36 +0530

    Add files via upload

---
+++ LeetcodeSum.java
@@ -0,0 +1,16 @@
+class LeetcodeSum{
+
+
+    static int singlenumber(int nums[]){
+        int value=0;
+        for(int i=0;i<nums.length;i++){
+            value =value^nums[i];
+        }
+        return value;
+    }
+    public static void main(String[] args) {
+       int[] nums={4,1,2,1,2};
+       System.out.println(singlenumber(nums));
+    
+    }
+}
